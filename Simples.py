b0a19ac4ceca25a5a2fe73271f0313e1266b3fb2
Danilo-Santana <santanadanilo576@gmail.com>
2023-05-11 17:50:43 -0300

    Primeiro commit

---
+++ Simples.py
@@ -0,0 +1,16 @@
+Operacao = float(input("1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão\nPara escolher a operação digite o número de acordo com a lista: "))
+
+a = float(input("Digite o primeiro número: "))
+
+b = float(input("Digite o segundo número: "))
+
+if(Operacao == 1):
+    print(a+b)
+elif(Operacao == 2):
+    print(a-b)
+elif(Operacao == 3):
+    print(a*b)
+elif(Operacao == 4):
+    print(a/b)
+    
+
